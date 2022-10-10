5c36981398b3cbd97ce936d3980886ce5c3c9c98
Juliana Lima <75941079+Jlima17@users.noreply.github.com>
2022-10-10 10:35:01 -0300

    Add files via upload

---
+++ Arrays/questao1.ts
@@ -0,0 +1,20 @@
+
+
+/* function p(num:number):boolean{
+    if(num % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let num: number = 17
+let numP: boolean = p(num);*/
+
+function p(numP: number): boolean{
+    return numP % 2 == 0;
+}
+
+const I = (numI: number)=>{
+    return numI % 2 != 0;
+}
