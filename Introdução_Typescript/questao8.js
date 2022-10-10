51dca978859baee9cb441e9b0ccd8c07f62d78fa
Juliana Lima <75941079+Jlima17@users.noreply.github.com>
2022-10-10 10:09:58 -0300

    Add files via upload

---
+++ Introdução_Typescript/questao8.js
@@ -0,0 +1,8 @@
+//strictNullChecks on
+function testJu(j) {
+    if (j == null) {
+    }
+    else {
+        console.log("Você é o que escolhe ser!");
+    }
+}
---
+++ Introdução_Typescript/questao8.ts
@@ -0,0 +1,8 @@
+//strictNullChecks on
+
+function testJu(j: number | null){
+    if (j == null){
+    }else{
+        console.log("Você é o que escolhe ser!")
+    }
+}
