f655e4b6c91487c4ffc96ae27b49b1b16b146b63
Juliana Lima <75941079+Jlima17@users.noreply.github.com>
2022-10-13 22:03:50 -0300

    Add files via upload

---
+++ Encapsulamento/Exercício_5/questao1.js
@@ -0,0 +1,16 @@
+var Calculator = /** @class */ (function () {
+    function Calculator(operating1, operating2) {
+        this.operating1 = operating1;
+        this.operating2 = operating2;
+    }
+    Calculator.prototype.sum = function () {
+        return this.operating1 + this.operating2;
+    };
+    Calculator.prototype.subtraction = function () {
+        return this.operating1 - this.operating2;
+    };
+    return Calculator;
+}());
+var c1 = new Calculator(17, 10);
+console.log(c1.sum());
+console.log(c1.subtraction());
